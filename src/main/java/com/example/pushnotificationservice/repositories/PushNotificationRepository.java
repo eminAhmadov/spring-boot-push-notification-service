e896ac65f20bc455ffc472a2c75fa903e53cc836
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-03-29 22:01:19 +0200

    Created Push Notification Repository

---
+++ src/main/java/com/example/pushnotificationservice/repositories/PushNotificationRepository.java
@@ -0,0 +1,15 @@
+package com.example.pushnotificationservice.repositories;
+
+import com.example.pushnotificationservice.entities.PushNotification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PushNotificationRepository extends PagingAndSortingRepository<PushNotification, Long> {
+    Page<PushNotification> findAll(Pageable pageable);
+    Page<PushNotification> findByUser_UserId(UUID userId, Pageable pageable);
+}
