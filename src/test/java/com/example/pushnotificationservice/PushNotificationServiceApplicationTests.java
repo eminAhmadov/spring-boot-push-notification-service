303ef056d0fa78ea9956af06ced69ac565cdee8c
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-03-29 19:56:16 +0200

    Initial commit

---
+++ src/test/java/com/example/pushnotificationservice/PushNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pushnotificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
