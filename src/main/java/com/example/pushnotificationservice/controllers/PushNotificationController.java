580f4a2b0a7b8d15ad5e5cc2ceda2e84035060c9
Emin Ahmadov <emin.ahmadov99@gmail.com>
2020-04-04 21:47:29 +0200

    Created PushNotification controller

---
+++ src/main/java/com/example/pushnotificationservice/controllers/PushNotificationController.java
@@ -0,0 +1,49 @@
+package com.example.pushnotificationservice.controllers;
+
+import com.example.pushnotificationservice.entities.PushNotification;
+import com.example.pushnotificationservice.services.PushNotificationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("pushNotification")
+public class PushNotificationController {
+
+    final
+    PushNotificationService pushNotificationService;
+
+    public PushNotificationController(
+            PushNotificationService pushNotificationService
+    ) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @GetMapping("/getAll")
+    public List<PushNotification> getAll(
+            @RequestParam(value = "offset", defaultValue = "0", required = false) int offset,
+            @RequestParam(value = "limit", defaultValue = "5", required = false) int limit
+    ) {
+        return pushNotificationService.getAll(offset, limit);
+    }
+
+    @GetMapping("/getAllForUserWithUserId")
+    public List<PushNotification> getAllForUserWithUserId(
+            @RequestParam UUID userId,
+            @RequestParam(value = "offset", defaultValue = "0", required = false) int offset,
+            @RequestParam(value = "limit", defaultValue = "5", required = false) int limit
+    ) {
+        return pushNotificationService.getAllForUserWithUserId(userId, offset, limit);
+    }
+
+    @PostMapping(path = "/send", consumes = "application/json", produces = "application/json")
+    public PushNotification sendPushNotification(
+            @RequestParam UUID userId,
+            @RequestBody Map<String, String> pushNotification) throws ParseException {
+        return pushNotificationService.createPushNotification(userId, pushNotification);
+    }
+
+}
